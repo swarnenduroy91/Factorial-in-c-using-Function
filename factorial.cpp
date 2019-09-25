51787bb32e10fbacd5a74c5bb7e38406951a8d1b
DeepikaPiku <deepikapadukonepiku@gmail.com>
2019-09-25 17:04:27 +0600

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,18 @@
+/*Finding Factorial Number*/
+
+#include <stdio.h>
+
+int factriol(int factriolNum){
+    int i;
+    int fact = 1;
+    for(i=1;i<=factriolNum;i++){
+        fact=fact*i;
+    }
+    return fact; 
+}
+int main() {
+	//code
+	int x=factriol(5);
+	printf("%d",x);
+	return 0;
+}
